49756e9e419a51a6a0afae75b8b4bf6915eb3be4
tsuyoshi.ogawa <ogawa@giftee.co>
2012-04-21 20:38:48 +0900

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milk::Application.config.secret_token = '9c00ea62ccf88ffe5c9150f3185f1feac38218220726c296cbb675d16f85185d1257ae2965a18517faa75d64e19bc0cdf2c07614261c47c30d334b53898f62a8'
